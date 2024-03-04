cd0e65d67c4cbfa6c6e2f6fe890fa194c895b847
EzequielBPullolil <programacionezequielk@gmail.com>
2024-03-04 18:32:33 -0300

    (feat) Now the host and port are passed by parameter to the binary

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	"github.com/EzequielBPullolil/cart_service/src"
 )
 
 func main() {
+	var host string
+	var port int
+
+	flag.StringVar(&host, "host", "localhost", "Host to listen on")
+	flag.IntVar(&port, "port", 8080, "Port to listen on")
+
+	flag.Parse()
 	app := src.CreateApp()
 
-	log.Fatal(app.Run(":8030"))
+	log.Fatal(app.Run(fmt.Sprintf("%s:%d", host, port)))
 }
